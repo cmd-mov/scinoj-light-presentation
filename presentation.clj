af518dc2093509f261dc54db0a0669a8952e92b7
Cvetomir Dimov <cvetomir.dimov@unige.ch>
2025-03-12 17:37:07 +0100

    Skeleton finished, presentation now runs again, introdution to CPS added, and practical implications

--- presentation.clj
+++ presentation.clj
@@ -6,11 +6,6 @@
 ;;     theme: dracula
 ;; ---
 
-;; To do:
-;; - small introduction to problem
-;; - practical implication
-;; - introduce linear regression
-
 ^{:kindly/hide-code true}
 (ns presentation
   (:require [tablecloth.api :as tc]
@@ -20,20 +15,40 @@
             [tech.v3.dataset.modelling :as ds-mod]
             [fastmath.ml.regression :as reg]))
 
-;; ## The Task: Tik Tik
+;; ## Collaborative Problem Solving
+;; 1. *What is it?*
+;; - a group goal that needs to be accomplished through problem solving
+;; - a single individual cannot solve the problem alone
+;; - no routine plan is available for the problem
+
+;; ## Collaborative Problem Solving
+;; 2. *How important is it?*
+;; - one of the **essential** 21st century skills (project Assesment and Teaching of 21st century skills)
+;; 3. *Why is it so important?*
+;; - many contemporary problems require teams of individuals with different expertise and background
+
+;; ## Collaborative Planning
+;; - envision alternative courses of action
+;; - select and commit to one of these courses of action
+;; - these can be jointly costructed and must be jointly agreed upon in the case of collaborative planning
+;; - the problem difficulty is a function of, among others, how many alternative courses of action there are
+
+;; ## Tik Tik: A Collaborative Planning Task
 ^{:kindly/hide-code true}
 (kind/hiccup 
  [:div {:style {:text-align "center"}}
-  (kind/video {:src "notebooks/videos/Demo.webm"})])
+  (kind/video {:src "notebooks/videos/Demo.webm"
+               :iframe-width 800
+               :iframe-height 560})])
 
 ;; ## Project goal
 ;; 1. Create a problem set, which is:
 ;; - within the general population's abilities
-;; - varies systemamically in difficulty
+;; - varies systematically in difficulty
 ;; - takes about an hour to complete
 ;; 2. Identify level parameters that determine level difficulty
 
-;; ## Designing levels
+;; ## Designing problems
 ^{:kindly/hide-code true}
 (kind/hiccup
  [:div {:style {:display "flex"}}
@@ -41,180 +56,123 @@
    [:image {:src "notebooks/images/Level103001.png"}]]
   [:div {:style {:flex "50%"}}
    [:image {:src "notebooks/images/Level119044.png"}]]])
-;; Number of barriers and number of barrier intersections.
+;; Factors that affect problem difficulty:
+^{:kindly/hide-code true}
+(kind/hiccup
+ [:ul
+  [:li "number of barriers and number of barrier intersections"]
+  [:li "start and goal locations"]])
 
-;; ## Problem creator and solver (in Clojure & Quil)
+;; ## Varying problem difficulty systematically
+;; - vary the minimum number of blockages necessary to solve a level (**optimal solution length**)
+;; - vary how many unique courses of action there are in a level (**search space size**)
+
+;; ## Problem creator and solver
 ^{:kindly/hide-code true}
 (kind/image
  {:src "notebooks/images/LevelCreator.png"})
 
 ;; ## Study 1
-;; - 29 levels: 9 warmup levels + 20 main levels (between 1 and 5 blockages)
-;; - 49 subjects (online platform Prolific)
-;; - about 1.5 hours of total time
+;; - *29 levels*: 9 warmup levels + 20 main levels (between 1 and 5 blockages)
+;; - *49 subjects* (online platform Prolific)
+;; - about *1.5 hours* of total time
+;; - subjects received *1 bonus point* for a correct solution and *3 bonus points* for an optimal solution
+;;
 
-;; ## Load data
 ^{:kindly/hide-code true}
 (def tiktik-data-study1
   (tc/dataset "notebooks/data/study1/study1_attempts.nippy"
               {:dataset-name "Tik Tik study 1 data"}))
+
 ^{:kindly/hide-code true}
-tiktik-data-study1
+(def subject-problem-summary
+  (-> tiktik-data-study1
+      (tc/group-by [:user :problem])
+      (tc/aggregate {:attempts tc/row-count
+                     :tot-duration #(reduce + (% :duration)) 
+                     :blocks-tot #(last (:blocks %))
+                     :solved? #(last (:success %))})
+      (tc/order-by [:user :problem])))
 
-;; ## Summarize by subject and level
-#_(def subject-level-summary
-    (-> tiktik-data
-        (tc/group-by [:user :level])
-        (tc/aggregate {:attempts tc/row-count
-                       :tot-duration #(reduce + (% :duration)) 
-                       :blocks-tot #(last (:blocks %))
-                       :solved? #(last (:success %))})
-        (tc/order-by [:user :level])))
-#_subject-level-summary
+^{:kindly/hide-code true}
+(def problems-characterization
+  (tc/dataset "notebooks/data/problem_characterization_studies12.nippy"))
 
-;; ## Loading level characterization
-#_(def levels-characterization
-(tc/dataset "levels_characterization.nippy"))
-
-#_(def subject-level-summary+
-(-> subject-level-summary
-    (tc/left-join levels-characterization [:level])
-    (tc/drop-columns [:levels_characterization.nippy.level])
-    (tc/eq :optimal? [:blocks-tot :optimal-solution-length])))
-#_(tc/column-names subject-level-summary+)
+^{:kindly/hide-code true}
+(def subject-problem-summary+
+  (-> subject-problem-summary
+      (tc/left-join problems-characterization [:problem])
+      (tc/eq :optimal? [:blocks-tot :optimal-solution-length])))
 
 ;; ## Subject performance
-#_(def subject-summary
-(-> subject-level-summary+
-    (tc/group-by [:user])
-    (tc/aggregate {:attempted-levels tc/row-count
-                   :solved #(tcc/sum (:solved? %))
-                   :solved-optimally #(tcc/sum (:optimal? %))})))
-#_subject-summary
+^{:kindly/hide-code true}
+(def subject-summary
+  (-> subject-problem-summary+
+      (tc/group-by [:user])
+      (tc/aggregate {:attempted-problems tc/row-count
+                     :solved #(tcc/sum (:solved? %))
+                     :solved-optimally #(tcc/sum (:optimal? %))})))
 
-;; ## Subject performance (cont'd)
-#_(-> subject-summary
-      (plotly/layer-point
-       {:=x :solved
-        :=y :solved-optimally
-        :=x-title "Number of problems solved"
-        :=y-title "Number of problems solved optimally"}))
+(-> subject-summary
+    (plotly/layer-point
+     {:=x :solved
+      :=y :solved-optimally
+      :=x-title "Number of problems solved"
+      :=y-title "Number of problems solved optimally"}))
 
-;; ## Level performance
-#_(def level-summary
-    (-> subject-level-summary+
-        (tc/group-by [:level])
-        (tc/aggregate {:attempted-by tc/row-count
-                       :median_attempts #(tcc/median (:blocks-tot %))
-                       :solved-optimally #(tcc/sum (:optimal? %))
-                       :median_t #(tcc/median (:tot-duration %))})
-        (tc// :fraction-optimal [:solved-optimally :attempted-by])
-        (tc/left-join levels-characterization [:level])
-        (tc/drop-columns [:levels_characterization.nippy.level])))
-#_(tc/column-names level-summary)
+;; ## Level difficulty
+^{:kindly/hide-code true}
+(def problem-summary
+  (-> subject-problem-summary+
+      (tc/group-by [:problem])
+      (tc/aggregate {:attempted-by tc/row-count
+                     :median_attempts #(tcc/median (:blocks-tot %))
+                     :solved-optimally #(tcc/sum (:optimal? %))
+                     :median-solution-time #(tcc/median (:tot-duration %))})
+      (tc// :optimal-solution-rate [:solved-optimally :attempted-by])
+      (tc/left-join problems-characterization [:problet])))
 
-;; ## Number of attempts and solution time
-#_(-> level-summary
-      (plotly/base {:=x :median_t
-                    :=y :median_attempts})
-      (plotly/layer-smooth {:=name "Linear fit"})
-      (plotly/layer-point {:=name "Data"
-                           :=y-title "Median number of attempts"
-                           :=x-title "Median time spent in level (s)"}))
-
-;; ## Optimal solution frequency and number of attempts
-#_(-> level-summary
-      (plotly/base {:=x :median_attempts
-                    :=y :fraction-optimal})
-      (plotly/layer-smooth {:=name "Linear fit"})
-      (plotly/layer-point {:=name "Data"
-                           :=y-title "Relative frequency of optimal solution"
-                           :=x-title "Median number of attempts"}))
+;; ^{:kindly/hide-code true}
+;; (-> problem-summary
+;;     (plotly/base {:=x :median-solution-time
+;;                   :=y :optimal-solution-rate})
+;;     (plotly/layer-smooth {:=name "Linear fit"})
+;;     (plotly/layer-point {:=name "Data"
+;;                          :=y-title "Median number of attempts"
+;;                          :=x-title "Median time spent in level (s)"}))
+;; TODO Maybe use solution rate and optimal solution rate
 
 ;; ## Summary on Level difficulty
-;; - The various measures of level difficulty highly correlate
-;; - We will use the frequecy of optimal solution as a measure of level difficulty
-;; - We still needs to find predictors of level difficulty, which include:
-;;   - optimal solution length
-;;   - search space size
-;;   - trimmed search space size
+;; - levels are solved optimally at rates between X and Y
+;; - very few levels are
 
-;; ## Number of blockages in optimal solution
-#_(-> level-summary
-      (plotly/base {:=y :fraction-optimal
-                    :=x :optimal-solution-length})
-      (plotly/layer-smooth {:=name "Linear fit"})
-      (plotly/layer-point {:=name "Data"
-                           :=y-title "Relative frequency of optimal solution"
-                           :=x-title "Optimal solution length"}))
+;; ## Which problem properties predict how difficult it is?
+;; We still needs to find predictors of level difficulty, which include:
+;;  - optimal solution length - the minimum number of blockages needed to solve a level
+;;  - search space size - the number of possible sequences of blockages that can be taken
+;;  - trimmed search space size - the search space size up to length equal to that of the optimal solution
 
-;; ## Search space size
-#_(-> level-summary
-      (plotly/base {:=y :fraction-optimal
-                    :=x :search-space})
-      (plotly/layer-smooth {:=name "Linear fit"})
-      (plotly/layer-point {:=name "Data"
-                           :=y-title "Relative frequency of optimal solution"
-                           :=x-title "Search space size"}))
+;; ## A quick introduction to logistic regression
+^{:kindly/hide-code true}
+(kind/hiccup
+ [:div {:style {:display "flex"}}
+  [:div {:style {:flex "50%"}}
+   [:ul
+    [:li "The logistic model is an S-shaped curve that ranges between 0 and 1"]
+    [:li "A logistic regression finds the logistic model that best describes a set of points"]]]
+  [:div {:style {:flex "50%"}}
+   [:image {:src "notebooks/images/LogisticCurve.jpg"}]]])
 
-;; ## Trimmed search space size
-#_(-> level-summary
-      (plotly/base {:=y :fraction-optimal
-                    :=x :trimmed-search-space})
-      (plotly/layer-smooth {:=name "Linear fit"})
-      (plotly/layer-point {:=name "Data"
-                           :=y-title "Relative frequency of optimal solution"
-                           :=x-title "Trimmed search space size"}))
+;; ## Predictors of level difficulty
 
-;; ## Load helper functions for linear regression
-#_(defn summary
-    "Generate a summary of a linear model."
-    [lmdata]
-    (kind/code
-     (with-out-str
-       (println
-        lmdata))))
-
-#_(defn lm
-    "Compute a linear regression model for `dataset`.
-  The first column marked as target is the target.
-  All the columns unmarked as target are the features.
-  The resulting model is of type `fastmath.ml.regression.LMData`,
-  created via [Fastmath](https://github.com/generateme/fastmath).
-  See [fastmath.ml.regression.lm](https://generateme.github.io/fastmath/clay/ml.html#lm)
-  for `options`."
-    ([dataset]
-     (lm dataset nil))
-    ([dataset options]
-     (let [inference-column-name (-> dataset
-                                     ds-mod/inference-target-column-names
-                                     first)
-           ds-without-target (-> dataset
-                                 (tc/drop-columns [inference-column-name]))]
-       (reg/lm
-        ;; ys
-        (get dataset inference-column-name)
-        ;; xss
-        (tc/rows ds-without-target)
-        ;; options
-        (merge {:names (-> ds-without-target
-                           tc/column-names
-                           vec)}
-               options)))))
-
-;; ## Linear regression
-#_(def mlr-model
-    (-> level-summary
-        (tc/select-columns [:fraction-optimal
-                            :optimal-solution-length
-                            :trimmed-search-space])
-        (ds-mod/set-inference-target :fraction-optimal)
-        lm))
-
-;; ## Linear regression (cont'd)
-#_(summary mlr-model)
-
-;; ## Conclusion
+;; ## Summary
 ;; - There are large individual differences in performance on Tik Tik
 ;; - Our problem set is within subject abilities, albeit a little difficult
-;; - The trimmed search space size is the strongest predictor of level difficulty, together with the number of blockages in optimal solution
+;; - The trimmed search space size is the strongest predictor of level difficulty, together with the optimal solution length
+
+;; ## Conclusion
+;; - A callibrated problem set will enable us to measure collaborative planning performance precisely
+;; - When combined with measures of various cognitive and social abilities, it will allow us to determine the relative contribution of these abilities.
+
+^{:kindly/hide-code true}
+(kind/hiccup [:small [:p "Made with: Quil (problem solver), Noj (data analysis), Clay (data analysis + presentation)"]])
